d886ce072f8c46f55d3064ea814a8dfa8560684e
HirayamaTatsuki <e115727@HirayamaTatsuki-no-MacBook-Pro-2.local>
2015-01-24 21:03:32 +0900

    stage 1

---
+++ Assets/StageObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageObject : MonoBehaviour {
+
+	GameObject Player;
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindWithTag ("Player") as GameObject;
+	}
+
+	void Starter(){
+
+		}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Mathf.Abs (Player.transform.position.x - transform.position.x) <= 3.0f)
+		{
+						renderer.enabled = true;
+				} 
+		else if (Mathf.Abs (Player.transform.position.z - transform.position.z) <= 3.0f) 
+		{
+						renderer.enabled = true;
+				} 
+		else 
+		{
+			renderer.enabled = false;
+				}
+	}
+}
